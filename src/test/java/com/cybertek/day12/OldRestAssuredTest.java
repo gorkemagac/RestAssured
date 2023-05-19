ba7c9adad66e2ad5769df5ac5fc0c3b1f8539936
Gorkem Agac <gorkem_agac@hotmail.com>
2023-05-20 00:05:30 +0100

    Day 12

---
+++ src/test/java/com/cybertek/day12/OldRestAssuredTest.java
@@ -0,0 +1,47 @@
+package com.cybertek.day12;
+
+import com.cybertek.utilities.SpartanNewBase;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class OldRestAssuredTest extends SpartanNewBase {
+
+
+    @Test
+    public void getAllSpartan()
+    {
+        given().accept(ContentType.JSON)
+                .and().auth().basic("admin","admin")
+                .log().all()
+                .when()
+                .get("/spartans")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id[0]",is(10))
+                .body("id[5]",is(199))
+                .log().all();
+    }
+    @Test
+    public void test2()
+    {
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .log().all()
+                .expect()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .body("id[0]",is(10))
+                .body("id[5]",is(199))
+                .logDetail(LogDetail.ALL)
+                .when()
+                .get("/spartans");
+    }
+}
